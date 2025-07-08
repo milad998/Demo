44e67aa54dadf18436806ca0349ce4a1e5f72d08
milad998 <myaralbwd20@gmail.com>
2025-07-08 16:34:21 +0300

    Update and rename M to User.kt

---
+++ demo/src/main/kotlin/com/example/milad/demo/model/User.kt
@@ -0,0 +1,17 @@
+package com.milad.demo.model
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false, unique = true)
+    val email: String
+)
