7c6b22e5e8bd289be03ba13b87471a257f044cfe
milad998 <myaralbwd20@gmail.com>
2025-07-08 17:06:27 +0300

    Update helloController.kt

--- demo/src/main/kotlin/com/example/milad/demo/controller/helloController.kt
+++ demo/src/main/kotlin/com/example/milad/demo/controller/helloController.kt
@@ -1,4 +1,8 @@
-@restController
+package com.example.milad.demo.controller
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
 @RequestMapping("/api")
 class HelloController{
   @GetMapping("/hello")
