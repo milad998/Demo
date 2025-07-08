5fbed681373528f081e7ea0add60a3ce474f62c3
milad998 <myaralbwd20@gmail.com>
2025-07-08 16:37:21 +0300

    Update and rename S to UserService.kt

---
+++ demo/src/main/kotlin/com/example/milad/demo/service/UserService.kt
@@ -0,0 +1,29 @@
+package com.milad.demo.service
+
+import com.milad.demo.model.User
+import com.milad.demo.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(private val userRepository: UserRepository) {
+
+    fun getAllUsers(): List<User> = userRepository.findAll()
+
+    fun getUserById(id: Long): User =
+        userRepository.findById(id).orElseThrow { NoSuchElementException("User not found") }
+
+    fun createUser(user: User): User = userRepository.save(user)
+
+    fun updateUser(id: Long, updatedUser: User): User {
+        val existingUser = getUserById(id)
+        val userToSave = existingUser.copy(
+            name = updatedUser.name,
+            email = updatedUser.email
+        )
+        return userRepository.save(userToSave)
+    }
+
+    fun deleteUser(id: Long) {
+        userRepository.deleteById(id)
+    }
+}
