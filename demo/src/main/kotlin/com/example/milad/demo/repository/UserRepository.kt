d54769508f4fe8b9b3fff02e71c2b42bbc0251ef
milad998 <myaralbwd20@gmail.com>
2025-07-08 16:35:32 +0300

    Update and rename R to UserRepository.kt

---
+++ demo/src/main/kotlin/com/example/milad/demo/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.milad.demo.repository
+
+import com.milad.demo.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>
