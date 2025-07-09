c56d01985c6c4ffd3f77be5b90188501278147a3
milad998 <myaralbwd20@gmail.com>
2025-07-09 21:54:18 +0300

    Create JwtService.kt

---
+++ demo/src/main/kotlin/com/example/milad/demo/security/JwtService.kt
@@ -0,0 +1,30 @@
+package com.example.milad.demo.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Service
+import java.util.*
+import javax.crypto.SecretKey
+
+@Service
+class JwtService {
+
+    private val secretKey: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+
+    fun generateToken(username: String): String {
+        return Jwts.builder()
+            .setSubject(username)
+            .signWith(secretKey)
+            .compact() // لا يوجد وقت انتهاء هنا (توكن دائم)
+    }
+
+    fun extractUsername(token: String): String {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body
+            .subject
+    }
+}
