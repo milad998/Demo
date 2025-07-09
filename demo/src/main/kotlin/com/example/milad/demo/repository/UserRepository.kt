ddd1b68782b3b34afba5ded66fe0b12318fbe012
milad998 <myaralbwd20@gmail.com>
2025-07-09 16:16:14 +0300

    Update UserRepository.kt

--- demo/src/main/kotlin/com/example/milad/demo/repository/UserRepository.kt
+++ demo/src/main/kotlin/com/example/milad/demo/repository/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.example.milad.demo.repository
 
-import com.milad.demo.model.User
+import com.example.milad.demo.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
