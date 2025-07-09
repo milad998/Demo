ea76a6e1e928891a3b63e9719802886226b5b80e
milad998 <myaralbwd20@gmail.com>
2025-07-09 21:51:17 +0300

    Update UserRepository.kt

--- demo/src/main/kotlin/com/example/milad/demo/repository/UserRepository.kt
+++ demo/src/main/kotlin/com/example/milad/demo/repository/UserRepository.kt
@@ -1,8 +1,9 @@
 package com.example.milad.demo.repository
 
+
 import com.example.milad.demo.model.User
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByUsername(username: String): User?
+}
