cb0c6e22563aeef1b0f7fea4f576d7338a1d70c1
milad998 <myaralbwd20@gmail.com>
2025-07-09 21:50:04 +0300

    Update User.kt

--- demo/src/main/kotlin/com/example/milad/demo/model/User.kt
+++ demo/src/main/kotlin/com/example/milad/demo/model/User.kt
@@ -1,17 +1,12 @@
 package com.example.milad.demo.model
 
 import jakarta.persistence.*
-import java.util.*
 
 @Entity
-@Table(name = "users")
 data class User(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-
-    @Column(nullable = false)
-    val name: String,
-
-    @Column(nullable = false, unique = true)
-    val email: String
+    @Column(unique = true)
+    val username: String = "",
+    val password: String = ""
 )
